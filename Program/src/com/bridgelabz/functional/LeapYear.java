56dcaf7b2caaaf7f1723137ba06f6f404906152b
Admin <bekiranabbi@gmail.com>
2019-01-07 19:16:12 +0530

    String replace added

---
+++ Program/src/com/bridgelabz/functional/LeapYear.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.functional;
+
+import com.brigelabz.utility.Utility;
+
+public class LeapYear {
+
+	public static void main(String[] args) 
+	{
+		Utility l=new Utility();
+		l.leapFind(2007);
+
+	}
+
+}
